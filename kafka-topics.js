e2864f85a6c4bb8b503f932f7b66f95c4f6d4cff
Trevor Scott <tscott@salesforce.com>
2018-11-19 13:50:40 -0800

    fix kafka-topics.js

--- kafka-topics.js
+++ kafka-topics.js
@@ -1,2 +1,8 @@
-export const CLICK_KAFKA_TOPIC     = process.env.CLICK_KAFKA_TOPIC || 'edm-ui-click,';
-export const PAGE_LOAD_KAFKA_TOPIC = process.env.PAGE_LOAD_KAFKA_TOPIC || 'edm-ui-pageload';
+const CLICK_KAFKA_TOPIC     = process.env.CLICK_KAFKA_TOPIC || 'edm-ui-click,';
+const PAGE_LOAD_KAFKA_TOPIC = process.env.PAGE_LOAD_KAFKA_TOPIC || 'edm-ui-pageload';
+
+
+module.exports = {
+  CLICK_KAFKA_TOPIC,
+  PAGE_LOAD_KAFKA_TOPIC
+}
